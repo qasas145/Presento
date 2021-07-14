10bf9ba2ed4140b16e6ca40e69ed7394af42eca8
muhammad elqasas <qasas145@gmail.com>
2021-07-15 01:04:45 +0200

    edited

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import React, {useEffect} from 'react';
 import ReactDOM from 'react-dom';
-import { BrowserRouter as Router , Switch, Link, Route} from "react-router-dom";
+import { BrowserRouter as Router , Switch, Link, Route, Redirect} from "react-router-dom";
 import Home from './Home'
 const App=()=>{
   return (
       <Router>
+        <Redirect to="/home"/>
           <Switch>
             <Route path="/home" component={Home} />
           </Switch>
